ebb0c909ce601cd011893163b504a7e0e3b11178
Matuki92 <matiaspierretti2@hotmail.com>
2020-04-11 10:30:57 +0200

    Add credits to me :)

--- src/javascript/Main.js
+++ src/javascript/Main.js
@@ -84,11 +84,12 @@ const main = (loaded_data) => {
 
   const build_splash = () => {
     splash_element = create_html(`<div id="splash_container">
-      <h1>Pepino Escape</h1>
-      <p>Cucumbers have invaded the planet <br> forcing Kiwi to leave. <br>
-      Avoid them by moving the pointer over the game screen.</p>
-      <input id="insert_name" placeholder="Insert Your Name Here" style="display:default">
-      <button id="start_button" style="display:none">Start Game</button>
+    <h1>Pepino Escape</h1>
+    <p>Cucumbers have invaded the planet <br> forcing Kiwi to leave. <br>
+    Avoid them by moving the pointer over the game screen.</p>
+    <input id="insert_name" placeholder="Insert Your Name Here" style="display:default">
+    <button id="start_button" style="display:none">Start Game</button>
+    <span id="byme"><p>by @matuki92</p></span>
   </div>`);
     main_content_element.appendChild(splash_element);
 
